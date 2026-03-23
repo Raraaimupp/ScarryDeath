1deb505fd8f06efa35ed027370edb91f12ac894e
Raraaa <raraasukaicecreamm@gmail.com>
2026-03-24 03:14:30 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  BOT_TOKEN: "TOKEN BOT LUU",
+  OWNER_ID: ["1886007660"], 
+  NOTIF_ID: "-1003223539193", 
+  domain: "https://",
+  plta: "ptla_",
+  pltc: "ptlc_",
+  eggid: "15",
+  location: "1",
+  nestid: "5",
+//  plta,pltc,domain,apidigitalocean ga di isi gpp kalau gapunya
+  do_api_key: "ISI_APIKEY_DIGITAL_OCEAN",
+};
