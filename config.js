eba4637edfe5291b8252171899907aec97db935f
Raraaimupp <raraasukaicecreamm@gmail.com>
2026-03-24 04:10:42 +0000

    extract file zip to real files

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  BOT_TOKEN: "TOKEN BOT KAMU",
+  OWNER_ID: ["ID TELEGRAM"],  
+  NOTIF_ID: "-1003223539193", 
+  domain: "https://",
+  plta: "ptla_",
+  pltc: "ptlc_",
+  eggid: "15",
+  location: "1",
+  nestid: "5",
+//  plta,pltc,domain,apidigitalocean ga di isi gpp kalau gapunya
+  do_api_key: "ISI_APIKEY_DIGITAL_OCEAN",
+};
